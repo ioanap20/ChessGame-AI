53fe0afe8071408699b57fed9e037fcb72a473bb
Ioana-Nicoleta PETAN <ioana-nicoleta.petan@polytechnique.edu>
2024-11-28 20:55:38 +0000

    Ioana

--- build/CMakeFiles/chess_proj.dir/reading.cpp.o.d
+++ build/CMakeFiles/chess_proj.dir/reading.cpp.o.d
@@ -1,5 +1,5 @@
-CMakeFiles/chess_proj.dir/main.cpp.o: \
- /mnt/c/Users/ioana/OneDrive/Documents/CScripts/CSE201/chess_proj/main.cpp \
+CMakeFiles/chess_proj.dir/reading.cpp.o: \
+ /mnt/c/Users/ioana/OneDrive/Documents/CScripts/CSE201/chess_proj/reading.cpp \
  /usr/include/stdc-predef.h /usr/include/c++/11/iostream \
  /usr/include/x86_64-linux-gnu/c++/11/bits/c++config.h \
  /usr/include/x86_64-linux-gnu/c++/11/bits/os_defines.h \
