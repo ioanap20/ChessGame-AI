8bfcb5abc05e9a60a3dfa6349c455ab6a08b94d7
Ioana-Nicoleta PETAN <ioana-nicoleta.petan@polytechnique.edu>
2024-12-05 08:12:44 +0000

    Ioana

--- reading.cpp
+++ reading.cpp
@@ -2,11 +2,26 @@
 #include <fstream>
 #include <string>
 #include <vector>
-
+#include <map>
+#include "reading.h"
 using namespace std;
 
+
 // Function to read all lines from the file
-vector<string> readAllLines(ifstream& infile) {
+vector<string> reading::readAllLines(int argc, char *argv[])
+{
+        // Should we check if the number of arguments is correct?
+    string historyFile = argv[2];
+    string outputFile = argv[4];
+
+    ifstream infile(historyFile);
+    ofstream fout(outputFile);
+
+    if (!infile.is_open() || !fout.is_open()) {
+        cerr << "Error: Unable to open input or output file." << endl;
+        return ;
+    }
+    
     vector<string> lines;
     string line;
 
@@ -17,39 +32,35 @@ vector<string> readAllLines(ifstream& infile) {
             lines.push_back(line); // Add line to vector
         }
     }
-
+    infile.close();
     return lines;
 }
 
-int main(int argc, char* argv[]) {
+void reading::update_white(vector<string> &whiteVector, vector<string> &moves)
+{
+    // update white vector
+    // go through all the moves and if the index is even, add the move to the white vector
+    // should we have a piece that we make piece.color = white?
+}
 
-    // Check if the number of arguments is correct?
+void reading::update_black(vector<string> &blackVector, vector<string> &moves)
+{
+    // update black vector
+    // go through all the moves and if the index is odd, add the move to the black vector
+    // should we have a piece that we make piece.color = black?
+}
 
-    string historyFile = argv[2];
-    string outputFile = argv[4];
-
-    ifstream infile(historyFile);
-    ofstream fout(outputFile);
-
-    if (!infile.is_open() || !fout.is_open()) {
-        cerr << "Error: Unable to open input or output file." << endl;
-        return 1;
-    }
-
-    // Read all lines from the input file
-    vector<string> allLines = readAllLines(infile);
-
-    // Count total lines
-    size_t lineCount = allLines.size();
-
-    // Determine color based on line count
-    string color = (lineCount % 2 == 1) ? "black" : "white";
+void reading::update_board(map<string, string> &boardMap)
+{
+    // update board
+}
 
 
-    fout << "New_move (" << color << ")" << endl;
-
-
-    infile.close();
-    fout.close();
-    return 0;
+// maybe this can be done another way i just wanted to have as little as possible in main
+void reading::actualize(map<string, string> &boardMap, vector<string> &whiteVector, vector<string> &blackVector, int argc, char* argv[])
+{
+    vector<string> allLines = readAllLines(argc, argv);
+    update_white(whiteVector, allLines);
+    update_black(blackVector, allLines);
+    update_board(boardMap);
 }
